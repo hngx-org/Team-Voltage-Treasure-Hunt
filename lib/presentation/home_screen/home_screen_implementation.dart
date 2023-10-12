8c8628499f3f030e21d7c7e9d00a414d1bc38129
VICTOR <vicsamoade@gmail.com>
2023-10-12 06:07:20 +0100

    dashboard card

--- lib/presentation/home_screen/home_screen_implementation.dart
+++ lib/presentation/home_screen/home_screen_implementation.dart
@@ -7,9 +7,51 @@ class Dashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xff82591B),
-      body: CategoryGridList(),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xff82591B),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
+              height: 180,
+              width: 320,
+              child: Row(
+                children: [
+                  // CircleAvatar(
+                  //   backgroundImage: AssetImage('assets/images/avatar.jpg'),
+                  //   radius: 50,
+                  // ),
+                  Column(
+                    children: [
+                      Text('points'),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text('skills'),
+                      Container(
+                        height: 10,
+                        width: 140,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Expanded(child: CategoryGridList()),
+          ],
+        ),
+      ),
     );
   }
 }
